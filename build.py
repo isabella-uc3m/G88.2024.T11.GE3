b1c02276572f41ece81e1aba4ef2faa361a18632
isabella-uc3m <159732930+isabella-uc3m@users.noreply.github.com>
2024-04-23 19:21:14 +0200

    Add files via upload

---
+++ build.py
@@ -0,0 +1,17 @@
+#   -*- coding: utf-8 -*-
+from pybuilder.core import use_plugin, init
+
+use_plugin("python.core")
+use_plugin("python.unittest")
+use_plugin("python.flake8")
+use_plugin("python.coverage")
+use_plugin("python.distutils")
+
+
+NAME = "EG2"
+DEFAULT_TASK = "publish"
+
+
+@init
+def set_properties(project):
+    pass
